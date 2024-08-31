8b8486306e87b7bc152741a47d2e0357678d0a15
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-08-31 18:18:34 +0900

    imgタグをNext.jsに対応

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -1,11 +1,18 @@
 import clsx from "clsx";
+import Image from "next/image";
 import { ComponentProps } from "react";
 import styles from "./Logo.module.css";
 
 export function Logo({ className, ...props }: ComponentProps<"div">) {
   return (
     <div className={clsx(className, styles.logo)} {...props}>
-      <img className={styles.mark} src={"/iruka.svg"} alt={"Logo"} />
+      <Image
+        className={styles.mark}
+        src="/iruka.svg"
+        alt="Logo"
+        width={32}
+        height={32}
+      />
       <span className={styles.wordmark}>Iruka Code</span>
     </div>
   );
