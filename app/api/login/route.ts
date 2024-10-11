0508947b201d122b3ceb50d720523131a1326f5c
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-10-11 22:28:02 +0900

    Next.jsのapiにlogin endpointを追加

---
+++ app/api/login/route.ts
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { NextRequest } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const { email, password } = await req.json();
+
+  if (!email || !password) {
+    const errorMessages = [];
+    if (!email) {
+      errorMessages.push("メールアドレスを入力してください");
+    }
+    if (!password) {
+      errorMessages.push("パスワードを入力してください");
+    }
+
+    return new Response(errorMessages.join(", "), { status: 400 });
+  }
+
+  const data = {
+    user: { email, password },
+  };
+
+  try {
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/login`,
+      data.user
+    );
+
+    if (response.status === 200) {
+      return new Response("ログインしました", { status: 200 });
+    }
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      if (error.response) {
+        if (error.response.data.errors) {
+          const errors = error.response.data.errors;
+          const errorsString = JSON.stringify(errors);
+          return new Response(errorsString, { status: 500 });
+        }
+        return new Response(error.response.data.message, { status: 500 });
+      }
+    }
+  }
+}
--- app/signin/DemoLogin.tsx
+++ app/signin/DemoLogin.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { signIn } from "next-auth/react";
 import { useState } from "react";
 import styles from "./signin.module.css";
 
@@ -8,28 +9,39 @@ export function DemoLogin() {
   const [password, setPassword] = useState("");
 
   async function valiDateSignIn() {
-    // try {
-    //   const response = await Axios.post(
-    //     `${process.env.NEXT_PUBLIC_API_URL}/api/login`,
-    //     {
-    //       email,
-    //       password,
-    //     },
-    //     {
-    //       withCredentials: true,
-    //     }
-    //   );
+    const url = `${process.env.NEXT_PUBLIC_ROUTE_API_URL}/api/login`;
 
-    //   if (response.status === 200) {
-    //     signIn("credentials", { email });
-    //   }
-    // } catch (error) {
-    //   console.error("Error fetching data:", error);
-    // }
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
 
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+    if (response.status === 400) {
+      const data = await response.text();
+      console.log(data);
+    }
 
-    console.log(res);
+    if (response.status === 500) {
+      const data = await response.text();
+
+      if (data === "Login information invalid") {
+        return;
+      }
+      const obj = JSON.parse(data);
+
+      const dataList = Object.keys(obj).map((key) => obj[key]);
+
+      dataList.forEach((error: string) => {
+        console.log(error);
+      });
+    }
+
+    if (response.ok) {
+      signIn("credentials", { email });
+    }
   }
 
   const handleSubmit = async (event: React.FormEvent) => {
